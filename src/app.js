cb3679fcd6a7f4d395911a4e1b66c4599a8573fc
brokentime906 <brokentime906@gmail.com>
2020-04-06 19:00:30 +0900

    cors options add

--- src/app.js
+++ src/app.js
@@ -10,7 +10,10 @@ var cors = require("cors");
 var indexRouter = require("./routes/index");
 var app = express();
 const webSocket = require("./socket");
-app.use(cors());
+const corsOptions = {
+  origin: "https://minichat.netlify.com",
+};
+app.use(cors(corsOptions));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
